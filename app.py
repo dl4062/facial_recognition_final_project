6e76e2234ab6afa11d1e56a9165932d4161f725b
dl4062 <dl4062@stern.nyu.edu>
2020-06-22 12:58:25 -0400

    luxand.cloud facial recoginition API setup
    - API key is stored in the .env file (which is ignored in gitignore)
    
    - API is set up and works now

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import os
 
+import requests
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+## pulling key from .env file
+API_KEY = os.environ.get("rapid_api_key")
+
+
+## code snippet taken from luxand.cloud facial recognition
+
+url = "https://luxand-cloud-face-recognition.p.rapidapi.com/photo/detect"
+
+payload = "photo=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F06%2FMorgan_Freeman%252C_2006_%2528cropped%2529.jpg"    ## add photo file name/path here - will need to connect to file structure eventually using code at the bottom
+headers = {
+    'x-rapidapi-host': "luxand-cloud-face-recognition.p.rapidapi.com",
+    'x-rapidapi-key': API_KEY,
+    'content-type': "application/x-www-form-urlencoded"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
+
+'''
 
 
 ## connecting code to folder stucture where picture data sits
@@ -23,3 +49,4 @@ while os.path.exists(os.path.join(data, choice)) == False:
 
 print(choice)
 
+'''
