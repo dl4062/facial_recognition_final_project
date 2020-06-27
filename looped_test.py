db2c3f70fb23d44d891fa5bcf13dfbd5d9de5ab8
dl4062 <dl4062@stern.nyu.edu>
2020-06-27 18:06:49 -0400

    test data and timer
    added test data and timer

--- looped_test.py
+++ looped_test.py
@@ -3,6 +3,7 @@ import json
 import requests
 import itertools
 from operator import itemgetter
+import time
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -28,6 +29,7 @@ for single_pic in picture_data:
         auth=(api_key, api_secret),
         files={'image': open(image_path, 'rb')})
     upload_response = json.loads(response.text)
+    #print(upload_response)
     upload = upload_response['result']['upload_id']
     image_upload_id = str(upload)
     request_url = f'https://api.imagga.com/v2/faces/detections?image_upload_id={image_upload_id}&return_face_attributes=1'
@@ -41,6 +43,7 @@ for single_pic in picture_data:
             gender_list.append(label)
         if label["type"] == "ethnicity":
             eth_list.append(label)
+    time.sleep(1)
 
 conf_gender_list = []
 conf_eth_list = []
@@ -63,61 +66,12 @@ print(conf_gender_list)
 
 
 
-'''
 
-        print("----------------------------")
-        print(label + ":")
-        for label in labels:
-           print(label["confidence"])
 
-breakpoint()
 
 ## https://docs.imagga.com/#uploads
-    
-#print(response.json())
-
-#upload_id = upload_response['upload_id']
-
-#print(upload)
-
-
-## adding in code from "practice.py" to test
 
 
 
 #https://api.imagga.com/v2/faces/detections?image_url=%s%https://upload.wikimedia.org/wikipedia/commons/0/06/Morgan_Freeman%2C_2006_%28cropped%29.jpg,auth=acc_9e32028a57242cd,59a2adc9efbe16b40d9df8132fe72c49
 
-
-#print(type(response)) --> requests.models.Response
-
-#changing data to dict from str
-
-
-#Data Investigation
-#print(type(parsed_response)) ---> dict
-#print(parsed_response.keys()) #---> result, status
-#print(parsed_response["result"])
-#print(parsed_response["result"].keys()) #--> "faces"
-#print(parsed_response["status"].keys()) #--> "text", "type"
-
-
-#pprint(results['faces'][0])
-
-
-#pprint(attribute)
-
-#print(type(attribute)) --> list
-
-
-
-#for t in attribute:
- #   type.append(t["type"])
-#pprint(type)
-
-#for c in attribute:
- #   print(c['confidence'])
-
-
-#pprint(sorted_attribute)
-#print(response.json())
-'''
